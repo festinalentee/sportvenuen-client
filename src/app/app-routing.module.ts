9b514f43f74c1daaf9b931fe0939b152797fd51f
festinalentee <karolina.bienkowska550@gmail.com>
2021-08-31 10:51:28 +0200

    Added some declarations and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./component/home/home.component";
 import {LoginComponent} from "./component/login/login.component";
 import {RegisterComponent} from "./component/register/register.component";
 import {AuthGuard} from "./guard/auth.guard";
 import {AdminComponent} from "./component/admin/admin.component";
-import {Role} from "./model/role";
 import {UserComponent} from "./component/user/user.component";
 import {AddVenueComponent} from "./component/add-venue/add-venue.component";
+import {ExploreComponent} from "./component/explore/explore.component";
+import {FavouritesComponent} from "./component/favourites/favourites.component";
+import {MyVenuesComponent} from "./component/my-venues/my-venues.component";
+import {MyBookingsComponent} from "./component/my-bookings/my-bookings.component";
+import {VenueComponent} from "./component/venue/venue.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/home'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'explore', component: ExploreComponent},
+  {path: 'venue', component: VenueComponent},
   {path: 'user', component: UserComponent, canActivate: [AuthGuard]},
-  {path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { role: [Role.ROLE_ADMIN]}},
-  {path: 'add-venue', component: AddVenueComponent},
+  {path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: {roles: {name: ['ROLE_ADMIN']}}},
+  {path: 'add-venue', component: AddVenueComponent, canActivate: [AuthGuard]},
+  {path: 'favourites', component: FavouritesComponent, canActivate: [AuthGuard]},
+  {path: 'my-venues', component: MyVenuesComponent, canActivate: [AuthGuard]},
+  {path: 'my-bookings', component: MyBookingsComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './component/home/home.component';
-import { LoginComponent } from './component/login/login.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NavigationComponent } from './component/navigation/navigation.component';
-import { RegisterComponent } from './component/register/register.component';
-import { AdminComponent } from './component/admin/admin.component';
-import { ErrorInterceptor } from "./interceptor/error.interceptor";
-import { JwtInterceptor } from "./interceptor/jwt.interceptor";
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-import { UserComponent } from './component/user/user.component';
-import { AddVenueComponent } from './component/add-venue/add-venue.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './component/home/home.component';
+import {LoginComponent} from './component/login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NavigationComponent} from './component/navigation/navigation.component';
+import {RegisterComponent} from './component/register/register.component';
+import {AdminComponent} from './component/admin/admin.component';
+import {ErrorInterceptor} from "./interceptor/error.interceptor";
+import {JwtInterceptor} from "./interceptor/jwt.interceptor";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {UserComponent} from './component/user/user.component';
+import {AddVenueComponent} from './component/add-venue/add-venue.component';
+import {ExploreComponent} from './component/explore/explore.component';
+import {FavouritesComponent} from './component/favourites/favourites.component';
+import {MyVenuesComponent} from './component/my-venues/my-venues.component';
+import {MyBookingsComponent} from './component/my-bookings/my-bookings.component';
+import {VenueComponent} from './component/venue/venue.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { AddVenueComponent } from './component/add-venue/add-venue.component';
     RegisterComponent,
     AdminComponent,
     UserComponent,
-    AddVenueComponent
+    AddVenueComponent,
+    ExploreComponent,
+    FavouritesComponent,
+    MyVenuesComponent,
+    MyBookingsComponent,
+    VenueComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +44,10 @@ import { AddVenueComponent } from './component/add-venue/add-venue.component';
     HttpClientModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
